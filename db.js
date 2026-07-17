7371b21d7fdd510bef725e1ef8675dc5f1e2a8a8
rishalsha <rishal.p786@gmail.com>
2026-07-17 15:31:07 +0530

    Configure SQLite to save to .data/ directory for production environments like Glitch

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'easybin.db');
+const dbDir = path.join(__dirname, '.data');
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
+const dbPath = path.join(dbDir, 'easybin.db');
 const db = new sqlite3.Database(dbPath);
 
 // Initialize DB schema
